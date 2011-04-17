dfd075023e6551f2c3bb6eaff0559f958b328b42
vileda <mail@tristan-leo.de>
2011-04-17 15:49:58 +0200

    html5 boilerplate integrated and http basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+  USERS = { "schweine" => "sperma" }
+  def authenticate
+    authenticate_or_request_with_http_digest("DieEwigeListe") do |name|
+      USERS[name]
+    end
+  end
 end
