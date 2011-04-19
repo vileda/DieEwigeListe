fb167bf4351bb89eadca771af15be869f0b410fa
vileda <mail@tristan-leo.de>
2011-04-19 22:23:26 +0200

    players now can upload an avatar

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,5 @@
 class Player < ActiveRecord::Base
   has_many :fixtures
   has_many :matches, :through => :fixtures
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :mini => "75x75>" }, :path => ":rails_root/public/system/:attachment/:id/:style/:filename"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110413184503) do
+ActiveRecord::Schema.define(:version => 20110418180852) do
 
   create_table "fixtures", :force => true do |t|
     t.integer  "player1_id"
@@ -31,6 +31,10 @@ ActiveRecord::Schema.define(:version => 20110413184503) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
